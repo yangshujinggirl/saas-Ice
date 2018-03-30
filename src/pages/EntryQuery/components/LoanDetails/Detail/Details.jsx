f30e947d6ce5505a1b15b74a014887d69042fecd
qianweiwei <qianweiwei@pingchang666.com>
2018-03-30 10:14:17 +0800

    详情页面附属字段渲染

--- src/pages/EntryQuery/components/LoanDetails/Detail/Details.jsx
+++ src/pages/EntryQuery/components/LoanDetails/Detail/Details.jsx
@@ -57,13 +57,35 @@ export default class Details extends Component {
           <IceContainer title={item.name} className='subtitle'  key={i}>
             <div className='config-font-box'>
               {item.fields.map((el,ind)=>{
-                return(
-                  <div className='config-font' key={el.id}>
-                    {this.label(el.label)}
-                    <span>:</span>
-                    {this.value(el.value)}
-                  </div>
-                )
+                var list = [];
+                if(el.hasAttachedFields){
+                  list.push( <div className='config-font colspan' key={el.id}>
+                                      {this.label(el.label)}
+                                      <span>:</span>
+                                      {this.value(el.value)}
+                                    </div>
+                            )
+                  if(el.attached[el.value]){
+                    el.attached[el.value].map((ite,ind)=>{
+                      list.push(
+                        <div className='config-font' key={item.id}>
+                          {this.label(item.label)}
+                          <span>:</span>
+                          {this.value(item.value)}
+                        </div>
+                      )
+                    })
+                  }
+                  return list;
+                }else{
+                  list.push( <div className='config-font ' key={el.id}>
+                      {this.label(el.label)}
+                      <span>:</span>
+                      {this.value(el.value)}
+                    </div>
+                  )
+                  return list;
+                }
               })
               }
             </div>
