00de45469628ae03cd8cf80e338e313a5def9627
haihaigang <haigang@haihaigang.local>
2018-04-10 21:21:19 +0800

    调整constant

--- src/pages/Product/reducers/ProductReducer.js
+++ src/pages/Product/reducers/ProductReducer.js
@@ -14,6 +14,7 @@ const index = (state = {
   },
   view: 'list' //展示界面类型，list列表、form表单、view详情等
 }, action) => {
+  console.log('productreducer')
   switch (action.type) {
     case T.FETCH_START:
       return { ...state, isFetching: true }
