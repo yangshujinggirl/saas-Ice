a8f0b552c47630457e32b6fd04c06f1ef090df37
qianweiwei <qianweiwei@pingchang666.com>
2018-04-11 09:35:25 +0800

    修改车贷申请的查询条件

--- src/pages/EntryQuery/EntryQuery.jsx
+++ src/pages/EntryQuery/EntryQuery.jsx
@@ -8,28 +8,6 @@ export default class EntryQuery extends Component {
     super(props);
     this.state = {};
   }
-  getLoanCode = (data) => {
-    this.state.code = data.code;
-    console.log(this.state.code);
-  }
-
-  changeComponent = (toggleComponent) => {
-    this.state.iscompont = toggleComponent
-    switch (this.state.iscompont) {
-      case 'FilterTable':
-        this.setState({
-          Component:<FilterTable {...this.props}/>
-        })
-        break;
-      case 'LoanDetails':
-        this.setState({
-          Component:<LoanDetails {...this.props}/>
-        })
-        break;
-      default:
-        break;
-    }
-  }
 
   render() {
     return (
--- src/pages/EntryQuery/components/EnterTable/Filter/Filter.jsx
+++ src/pages/EntryQuery/components/EnterTable/Filter/Filter.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, Input, Grid, Select, Button, DatePicker } from '@icedesign/base';
+import { Form, Input, Grid, Select, Button, DatePicker,Feedback} from '@icedesign/base';
 
 // form binder 详细用法请参见官方文档
 import {
@@ -10,6 +10,7 @@ import {
 const { Row, Col } = Grid;
 const { Option } = Select;
 const FormItem = Form.Item;
+const Toast = Feedback.toast;
 
 const formItemLayout = {
   labelCol: {
@@ -22,7 +23,16 @@ const formItemLayout = {
 
 export default class Filter extends Component {
   static displayName = 'Filter';
-
+  submit = () =>{
+    // this.props.onSubmit();
+    console.log(this.props.value)
+    let {endTime, startTime } = this.props.value
+    console.log(endTime)
+    console.log(startTime)
+    if(endTime.getTime() <= startTime.getTime()){
+      Toast.help("申请开始时间不能大于申请结束时间");
+    }
+  }
   render() {
     return (
       <IceFormBinderWrapper
@@ -104,7 +114,7 @@ export default class Filter extends Component {
               </Col>
               <Col s="8" l="8">
                 <FormItem {...formItemLayout} label="&nbsp;">
-                  <Button type="secondary" htmlType='submit' onClick={this.props.onSubmit}>
+                  <Button type="secondary" htmlType='submit' onClick={this.submit}>
                     查询
                   </Button>
                 </FormItem>
