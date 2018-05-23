719ce05f2f509b73ff4933a5796e552bd8f195e1
wanghaigang <wanghaigang@pingchang666.com>
2018-05-23 19:02:15 +0800

    重构actions

--- src/pages/Process/actions/ProcessAction_.js
+++ src/pages/Process/actions/ProcessAction_.js
@@ -1,45 +1,41 @@
 import T from '../constants/ProcessConstant'
 import Req from '../reqs/ProcessReq'
+import { BaseAction } from 'base';
 import { Feedback } from "@icedesign/base";
 import { hashHistory } from 'react-router'
 import FontConfigReq from '../../FontConfig/reqs/FontConfigReq';
-import { BaseAction } from 'base';
-
-/*******以下定义需要通知到reduucer的全局方法，约定返回数据包括类型＋其余参数*******/
 
 class ProcessAction extends BaseAction {
 
   constructor() {
     super(Req, null, T);
 
-    // 定义通知给reducer的方法
-    this.actions = {
-      // search: this.search.bind(this)
-    };
+    // 定义通知给reducer的方法名称
+    this._actionKeys = [
+      'search',
+      'getCustomMenuList',
+      'save',
+      'changeHasProcess',
+      'getDetail',
+      'remove',
+      'copyProcess',
+      'getProcessProdList',
+      'getProcessProdOldList',
+      'saveProcessConfigProduct',
+      'getTasksFields',
+      'getPageFields',
+      'getAllPageFields',
+      'getPageDetail',
+      'getPrivilegeOrgs',
+      'changeFormData'
+    ];
 
-    this.defineAction('search', (condition) => {
-      return (dispatch) => {
-
-        dispatch(this.fetchStart({ formData: {} }))
-
-        Req.search(condition).then((res) => {
-          if (res.code == 200) {
-            dispatch(this.fetchSuccess({ pageData: res.data }))
-          }
-        }).catch((ex) => {
-          dispatch(this.fetchFailed(ex))
-        })
-      }
-    });
+    // 初始化一些数据
+    this.init();
   }
-
-  defineAction(key, func) {
-    this.actions[key] = func;
-  }
-
 
   // 获取列表
-  static search(condition) {
+  search(condition) {
     return (dispatch) => {
 
       dispatch(this.fetchStart({ formData: {} }))
@@ -53,6 +49,214 @@ class ProcessAction extends BaseAction {
       })
     }
   }
+
+  // 保存表单
+  save(data) {
+    return (dispatch) => {
+
+      dispatch(this.fetchStart({ isSubmiting: true }))
+
+      Req.save(data).then((res) => {
+        // 提交成功后弹框提示“xxx产品流程已提交成功”，停留2秒后自动消失，在跳转到列表
+        Feedback.toast.show({
+          type: 'success',
+          content: data.processName + '产品流程已' + (data.status == 1 ? '提交' : '保存') + '成功',
+          afterClose: () => {
+            dispatch(this.fetchSuccess({ formData: {}, isSubmiting: false }))
+            hashHistory.push('/process');
+          },
+          duration: 2000
+        });
+      }).catch((res) => {
+        dispatch(this.fetchFailed({ isSubmiting: false }))
+      })
+    }
+  }
+
+  // 获取详情
+  getDetail(id) {
+    return (dispatch) => {
+
+      dispatch(this.fetchStart({ formData: {} }))
+
+      Req.getDetail(id).then((res) => {
+        dispatch(this.fetchSuccess({ formData: res.data }))
+      }).catch((ex) => {
+        dispatch(this.fetchFailed(ex))
+      })
+    }
+  }
+
+  // 获取左侧
+  getCustomMenuList(id) {
+    return (dispatch) => {
+      dispatch(this.fetchStart({ customMenuList: [] }))
+      Req.getCustomMenuList(id).then((res) => {
+        if (res.code != 200) return;
+        dispatch(this.fetchSuccess({ customMenuList: res.data }))
+      }).catch((ex) => {
+        dispatch(this.fetchFailed(ex))
+      })
+    }
+  }
+
+  // 删除一条记录
+  remove(id) {
+    return (dispatch) => {
+
+      dispatch(this.fetchStart())
+
+      Req.delete(id).then((res) => {
+        dispatch(this.fetchSuccess({ delete: true }))
+      }).catch((ex) => {
+        dispatch(this.fetchFailed(ex))
+      })
+    }
+  }
+
+  // 复制流程
+  copyProcess(id) {
+    return (dispatch) => {
+
+      dispatch(this.fetchStart())
+
+      Req.copyProcess(id).then((res) => {
+        if (res.code != 200) {
+          Feedback.toast.show({
+            type: 'error',
+            content: res.msg,
+          });
+          return;
+        }
+        dispatch(this.fetchSuccess({ copy: true }))
+        hashHistory.push(`process/edit/${res.data.id}`);
+      }).catch((ex) => {
+        dispatch(this.fetchFailed(ex))
+      })
+    }
+  }
+
+  // 更改标志
+  changeHasProcess(hasProcess) {
+    return (dispatch) => {
+      dispatch(this.change({ hasProcess }))
+    }
+  }
+  //流程配置产品左侧列表
+  getProcessProdList(condition) {
+    return (dispatch) => {
+
+      Req.getProcessProdList(condition).then((res) => {
+        if (res.code == 200) {
+          dispatch(this.fetchSuccess({ formData: res.data }))
+        }
+      }).catch((ex) => {
+        dispatch(this.fetchFailed(ex))
+      })
+    }
+  }
+  //流程配置产品右侧列表 
+  getProcessProdOldList(id) {
+    return (dispatch) => {
+
+      Req.getProcessProdOldList(id).then((res) => {
+        if (res.code == 200) {
+          dispatch(this.fetchSuccess({ formLeftData: res.data }))
+
+        }
+      }).catch((ex) => {
+        dispatch(this.fetchFailed(ex))
+      })
+    }
+  }
+  //流程配置产品保存
+  saveProcessConfigProduct(data, id) {
+    return (dispatch) => {
+
+      dispatch(this.fetchStart())
+
+      Req.saveProcessConfigProduct(data, id).then((res) => {
+        hashHistory.push('/process')
+      }).catch((ex) => {
+        dispatch(this.fetchFailed(ex))
+      })
+    }
+  }
+  //获取必要字段
+  getTasksFields(taskTypeId) {
+    return (dispatch) => {
+      dispatch(this.fetchStart())
+
+      Req.getTasksFields(taskTypeId).then((res) => {
+        if (res.code != 200) return;
+        dispatch(this.fetchSuccess({ tasksFields: res.data }))
+      }).catch((ex) => {
+        dispatch(this.fetchFailed(ex))
+      })
+    }
+  }
+
+
+  //获取页面配置字段
+  getPageFields(options) {
+    return (dispatch) => {
+      dispatch(this.fetchStart())
+
+      FontConfigReq.getDetail(options).then((res) => {
+        if (res.code != 200) return;
+        dispatch(this.fetchSuccess({ pageFields: { fieldset: res.data.list } }))
+      }).catch((ex) => {
+        dispatch(this.fetchFailed(ex))
+      })
+    }
+  }
+
+  //获取页面所有的配置字段
+  getAllPageFields(options) {
+    return (dispatch) => {
+      dispatch(this.fetchStart())
+
+      FontConfigReq.getDetail(options).then((res) => {
+        if (res.code != 200) return;
+        dispatch(this.fetchSuccess({ allPageFields: { fieldset: res.data.list } }))
+      }).catch((ex) => {
+        dispatch(this.fetchFailed(ex))
+      })
+    }
+  }
+
+  //获取页面配置详情
+  getPageDetail(id) {
+    return (dispatch) => {
+      dispatch(this.fetchStart())
+
+      FontConfigReq.getCode(id).then((res) => {
+        if (res.code != 200) return;
+        dispatch(this.fetchSuccess({ pageFields: res.data }))
+      }).catch((ex) => {
+        dispatch(this.fetchFailed(ex))
+      })
+    }
+  }
+  //权限编辑机构／角色
+  getPrivilegeOrgs() {
+    return (dispatch) => {
+      dispatch(this.fetchStart())
+
+      Req.getPrivilegeOrgs().then((res) => {
+        // if (res.code != 200) return;
+        dispatch(this.fetchSuccess({ orgsData: res.data }))
+      }).catch((ex) => {
+        dispatch(this.fetchFailed(ex))
+      })
+    }
+  }
+
+  changeFormData(data) {
+    return (dispatch) => {
+      dispatch(change({ formData: data }))
+    }
+  }
 }
 
-export default ProcessAction;
+export default new ProcessAction().actions;
--- src/pages/Process/index.js
+++ src/pages/Process/index.js
@@ -4,10 +4,10 @@ import ProcessDetail from './containers/ProcessDetail'
 import ProcessConfig from './containers/ProcessConfig'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import * as ProcessActions from './actions/ProcessAction.js'
-// import ProcessActions from './actions/ProcessAction_.js'
+// import * as ProcessActions from './actions/ProcessAction.js'
+import ProcessActions from './actions/ProcessAction_.js'
 
-console.log(ProcessActions);
+// console.log(ProcessActions);
 
 const mapStateToProps = (state, ownProps) => {
     const data = state.ProcessReducer;
