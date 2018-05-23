d7442ae89547fcbb7f786254369e6b35632d5b2d
wanghaigang <wanghaigang@pingchang666.com>
2018-05-23 14:22:16 +0800

    添加一个发布中提示

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import 'core-js';
 import ReactDOM from 'react-dom';
 // 载入默认全局样式 normalize 、.clearfix 和一些 mixin 方法等
 import routes from './routes';
+import DeepApp from './base/third/DeepApp.jsx';
 
 let app = new DeepApp();
-console.log(app)
 
 // 以下代码 ICE 自动生成, 请勿修改
 // const container = document.createElement('div');
