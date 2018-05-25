3069e030fadd5af46db4f43fe1da95e981f61aa2
qianweiwei <qianweiwei@pingchang666.com>
2018-05-25 13:27:14 +0800

    面签

--- src/pages/InterView/reqs/InterViewReq.js
+++ src/pages/InterView/reqs/InterViewReq.js
@@ -9,7 +9,7 @@ class InterViewReq extends CurdReq{
 		this.curd = {
             create: this._host + '/filter-table-list.json',
             update: this._host + '/filter-table-list.json',
-            retrieve: this._host + '/filter-table-list.json',
+            retrieve: this._config.CONTRACT_HOST + `/interview/list`,
             delete: this._host + '/detail.json',
             detail: this._host + '/detail.json'
         }
@@ -37,6 +37,14 @@ class InterViewReq extends CurdReq{
 		}
 		return super.fetchData(options);
 	}
+  getInterViewDetail(id){
+    let options = {
+      url: this._config.CONTRACT_HOST + `/visaInterview/chinate/detail/${id}`,
+      method: 'get',
+      contentType: 'application/x-www-form-urlencoded',
+    }
+    return super.fetchData(options);
+  }
 }
 
 export default new InterViewReq();
